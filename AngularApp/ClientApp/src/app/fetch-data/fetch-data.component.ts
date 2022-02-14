0d1745677c32e5a89d36b4c3d1adb259549d396f
li-rongcheng <Li.Rongcheng@gmail.com>
2022-02-15 09:13:30 +1100

    delete app.server.module.ts, which seems to be useless

--- AngularApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ AngularApp/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+/**
+ * This component demonstrate fetching data via an REST API from a server.
+ */
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
@@ -8,6 +11,11 @@ import { HttpClient } from '@angular/common/http';
 export class FetchDataComponent {
   public forecasts: WeatherForecast[] = [];
 
+  /**
+   * @param http An HttpClient instance
+   * @param baseUrl An injected string from 'BASE_URL'
+   * @returns There's no return.
+   */
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
     http.get<WeatherForecast[]>(baseUrl + 'weatherforecast').subscribe(result => {
       this.forecasts = result;
@@ -15,7 +23,13 @@ export class FetchDataComponent {
   }
 }
 
+/**
+ * The interface of WeatherForecast object returned from server API /weatherforecast
+ */
 interface WeatherForecast {
+  /**
+   * 
+   */
   date: string;
   temperatureC: number;
   temperatureF: number;
