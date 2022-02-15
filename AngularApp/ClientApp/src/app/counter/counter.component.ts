1947fd47c518bac83f7f87b72f1aa91e710c47c1
li-rongcheng <Li.Rongcheng@gmail.com>
2022-02-16 01:14:19 +1100

    apply BehaviorSubject and Observable

--- AngularApp/ClientApp/src/app/counter/counter.component.ts
+++ AngularApp/ClientApp/src/app/counter/counter.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { CounterService } from './counter.service';
 
 /**
@@ -9,19 +10,31 @@ import { CounterService } from './counter.service';
 @Component({
   selector: 'app-counter-component',
   templateUrl: './counter.component.html',
-  providers: [ CounterService ]
+  // providers: [ CounterService ]
 })
-export class CounterComponent implements OnInit {
+export class CounterComponent implements OnInit, OnDestroy {
   public currentCount = 0;
+  private subscription?: Subscription;
 
   constructor(private counterService: CounterService) { }
 
   ngOnInit(): void {
     this.currentCount = this.counterService.getCount();
+    this.subscription = this.counterService.count$.subscribe(
+      res => {
+        this.currentCount = res;
+        console.log(`currentCount value updated to ${res}`);
+      },
+      err => { console.log(`An error occurred: ${err.message}`); }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+    console.log(`subscription unsubscribed`);
   }
 
   public incrementCounter() {
-    this.counterService.increaseCount();
-    this.currentCount = this.counterService.getCount();
+    this.counterService.increaseCount(11);
   }
 }
--- AngularApp/ClientApp/src/app/counter/counter.service.ts
+++ AngularApp/ClientApp/src/app/counter/counter.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CounterService {
 
-  private count: number = 10;
+  private _count$: BehaviorSubject<number> = new BehaviorSubject<number>(10);
+  readonly count$: Observable<number> = this._count$.asObservable();
 
   constructor() { }
 
-  public getCount():number { return this.count; }
-  public increaseCount() { this.count += 10; }
+  public getCount(): number {return this._count$.getValue(); }
+  public increaseCount(amount: number): void { this._count$.next(this._count$.getValue() + amount); }
 }
