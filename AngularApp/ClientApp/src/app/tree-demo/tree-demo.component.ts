5a45e1330d3c1c2e0df5f45c0b37356afc9503cf
li-rongcheng <Li.Rongcheng@gmail.com>
2022-02-28 09:59:16 +1100

    add tree-demo implementation

--- AngularApp/ClientApp/src/app/tree-demo/tree-demo.component.ts
+++ AngularApp/ClientApp/src/app/tree-demo/tree-demo.component.ts
@@ -1,4 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { FlatTreeControl } from '@angular/cdk/tree';
+import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Fruit',
+    children: [
+      { name: 'Apple' },
+      { name: 'Banana' },
+      { name: 'Fruit loops' },
+    ]
+  }, {
+    name: 'Vegetables',
+    children: [
+      {
+        name: 'Green',
+        children: [
+          { name: 'Broccoli' },
+          { name: 'Brussels sprouts' },
+        ]
+      }, {
+        name: 'Orange',
+        children: [
+          { name: 'Pumpkins' },
+          { name: 'Carrots' },
+        ]
+      },
+    ]
+  },
+];
 
 @Component({
   selector: 'app-tree-demo',
@@ -7,7 +48,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreeDemoComponent implements OnInit {
 
-  constructor() { }
+  private _transformer = (node: FoodNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  }
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level, node => node.expandable);
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer, node => node.level, node => node.expandable, node => node.children);
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+   }
 
   ngOnInit(): void {
   }
