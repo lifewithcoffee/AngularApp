3bf0f8600ca8633200107a54218e38ca6bd4fb5c
li-rongcheng <Li.Rongcheng@gmail.com>
2022-03-01 02:21:38 +1100

    tidy up

--- AngularApp/ClientApp/src/app/tree-demo/tree-demo.component.ts
+++ AngularApp/ClientApp/src/app/tree-demo/tree-demo.component.ts
@@ -21,7 +21,8 @@ const TREE_DATA: FoodNode[] = [
       { name: 'Banana' },
       { name: 'Fruit loops' },
     ]
-  }, {
+  },
+  {
     name: 'Vegetables',
     children: [
       {
@@ -48,6 +49,9 @@ const TREE_DATA: FoodNode[] = [
 })
 export class TreeDemoComponent implements OnInit {
 
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
+
+  // _note_: transfer plain data node (FoodNode) to flat node (ExampleFlatNode)
   private _transformer = (node: FoodNode, level: number) => {
     return {
       expandable: !!node.children && node.children.length > 0,
@@ -56,15 +60,9 @@ export class TreeDemoComponent implements OnInit {
     };
   }
 
-  treeControl = new FlatTreeControl<ExampleFlatNode>(
-    node => node.level, node => node.expandable);
-
-  treeFlattener = new MatTreeFlattener(
-    this._transformer, node => node.level, node => node.expandable, node => node.children);
-
+  treeControl = new FlatTreeControl<ExampleFlatNode>(node => node.level, node => node.expandable); // _note_: the tree control
+  treeFlattener = new MatTreeFlattener(this._transformer, node => node.level, node => node.expandable, node => node.children);
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
-
-  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 
   constructor() {
     this.dataSource.data = TREE_DATA;
@@ -73,4 +71,12 @@ export class TreeDemoComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // _note_: ngAfterViewInit()
+  ngAfterViewInit() {
+    console.table(this.treeControl.dataNodes); // _note_: view the log output to understand why expand dataNodes 4 and 8
+
+    this.treeControl.expand(this.treeControl.dataNodes[0]);   // _note_: call treeControl.expand()
+    this.treeControl.expand(this.treeControl.dataNodes[4]);
+    this.treeControl.expand(this.treeControl.dataNodes[8]);
+  }
 }
