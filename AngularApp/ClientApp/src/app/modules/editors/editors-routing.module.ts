1827f2d27c30d5fe35805b98cf4601f714f1edae
li-rongcheng <Li.Rongcheng@gmail.com>
2022-03-03 21:57:29 +1100

    add routes and nav button

--- AngularApp/ClientApp/src/app/modules/editors/editors-routing.module.ts
+++ AngularApp/ClientApp/src/app/modules/editors/editors-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditorsComponent } from './pages/editors/editors.component';
 import { QuillEditorComponent } from './pages/quill-editor/quill-editor.component';
 
 const routes: Routes = [
-  { path: 'quill-editor', component: QuillEditorComponent },
+  { path: 'editors', component: EditorsComponent, children:[
+    { path: 'quill-editor', component: QuillEditorComponent },
+  ]},
 ];
 
 @NgModule({
--- AngularApp/ClientApp/src/app/modules/editors/editors.module.ts
+++ AngularApp/ClientApp/src/app/modules/editors/editors.module.ts
@@ -3,14 +3,18 @@ import { CommonModule } from '@angular/common';
 
 import { EditorsRoutingModule } from './editors-routing.module';
 import { QuillEditorComponent } from './pages/quill-editor/quill-editor.component';
+import { EditorsComponent } from './pages/editors/editors.component';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
   declarations: [
-    QuillEditorComponent
+    QuillEditorComponent,
+    EditorsComponent
   ],
   imports: [
     CommonModule,
+    MatButtonModule,
     EditorsRoutingModule
   ]
 })
